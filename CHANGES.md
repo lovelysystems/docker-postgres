5de8f17eb1823dd68aef0c1b2721cf8fa4150807
dobe <bernddorn@gmail.com>
2021-10-12 16:42:13 +0200

    base project on postgres14 and added upgrade docker image

--- CHANGES.md
+++ CHANGES.md
@@ -4,6 +4,8 @@
 
 ### Feature
 
+- upgraded to postgresql 14.0
+- added upgrade docker image variant to upgrade from 12.x versions
 - added debugger extension
 
 ## 2020-04-29 / 0.1.0
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("com.lovelysystems.gradle") version ("1.6.0")
+    id("com.lovelysystems.gradle") version ("1.6.1")
 }
 
 lovely {
     gitProject()
-    dockerProject("lovelysystems/docker-postgres")
+    dockerProject("lovelysystems/docker-postgres", stages = listOf("", "upgrade12"))
 
     with(dockerFiles) {
         from("docker")
