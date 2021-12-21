0d9fc4068c861e432dc69f924094916a5bbfa206
dobe <bernddorn@gmail.com>
2021-12-21 11:39:09 +0100

    do not use docker compose v2 for linux compat

--- examples/pg_devsetup/build.gradle.kts
+++ examples/pg_devsetup/build.gradle.kts
@@ -3,14 +3,14 @@ val check by tasks.registering {
     dependsOn(":buildDockerImage")
     doLast {
         exec {
-            commandLine("docker", "compose", "up", "-d")
+            commandLine("docker-compose", "up", "-d")
         }
         exec {
-            commandLine("docker", "compose", "run", "--rm", "client", "wait_for_postgres")
+            commandLine("docker-compose", "run", "--rm", "client", "wait_for_postgres")
         }
 
         exec {
-            commandLine("docker", "compose", "run", "--rm", "client", "psql", "-c", "select 1")
+            commandLine("docker-compose", "run", "--rm", "client", "psql", "-c", "select 1")
         }
     }
 }
