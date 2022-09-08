d8c7c4cd329cd54f514e2c0971885a26d9650de4
Michael Beer <mb@outlook.at>
2022-09-08 11:59:42 +0200

    Add notify if fails slack monitoring of barman backup process

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Generating cron schedules"
-echo "${BARMAN_BACKUP_SCHEDULE} barman /usr/local/bin/barman backup --wait all" >> /etc/cron.d/barman
+echo "${BARMAN_BACKUP_SCHEDULE:-0 4 * * *} barman /usr/bin/notify_if_fails '/usr/bin/barman backup --wait all' '${NOTIFY_BACKUP_ERROR_MESSAGE:-Backup failed for $PGUSER@$PGHOST}'" >> /etc/cron.d/barman
 
 echo "Initializing done"
 
