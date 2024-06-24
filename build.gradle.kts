4bc4ed145312099d32252b9e1923c71568e8a0d4
Adam Kobor <28044849+adamkobor@users.noreply.github.com>
2024-06-24 11:35:11 +0200

    use MP apgdiff (#15)
    * use MP apgdiff
    
    * use latest 2404 ubuntu image on CircleCI
    
    * use apgdiff 1.0.1 & adjust changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.lovelysystems.gradle") version ("1.12.0")
+    id("com.lovelysystems.gradle") version ("1.13.0")
 }
 
 lovely {
@@ -7,15 +7,7 @@ lovely {
     dockerProject(
         "lovelysystems/docker-postgres",
         stages = listOf("", "backup", "client", "upgrade12to16"),
-        // note that the platform is nailed as AMD64 here, because M1 Macs would generate ARM64 images by default, but,
-        // when ARM64 is used, `diffdb` bumps into the following error when it runs the `apgdiff` command:
-        // "qemu-x86_64: Could not open '/lib64/ld-linux-x86-64.so.2': No such file or directory"
-        // That's because the apgdiff tool is only available within an AMD64 image, and even though it's copied
-        // successfully to an ARM64 image build time, it still won't work.
-        platforms = listOf("linux/amd64"),
-        buildPlatforms = listOf("linux/amd64")
     ) {
         from("docker")
     }
 }
-
