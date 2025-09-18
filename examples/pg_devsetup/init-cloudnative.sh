6fece7e9dbf27e24a61e1e92e6844d602f6be51f
Lukas-Kaufmann <kaufmann@lovelysystems.com>
2025-09-18 10:07:00 +0200

    revert

--- examples/pg_devsetup/init-cloudnative.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-PGDATA="${PGDATA:-/var/lib/postgresql/data}"
-PGUSER="${PGUSER:-postgres}"
-# Ensure correct ownership
-chown -R $PGUSER:$PGUSER "$PGDATA"
-postgres -c shared_preload_libraries=plugin_debugger
