192cd5ab76e3637042525a031044b0fd9d8db97d
Lukas-Kaufmann <kaufmann@lovelysystems.com>
2025-09-18 08:42:24 +0200

    change to postgres for init image

---
+++ examples/pg_devsetup/pg-init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+if [ ! -f "$PGDATA/PG_VERSION" ]; then
+  gosu postgres initdb --username=postgres -D "$PGDATA"
+  echo "initialized PGDATA at $PGDATA"
+  echo "host all all 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
+else
+  echo "PGDATA already initialized."
+fi
